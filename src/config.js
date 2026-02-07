11f54680db99ab58618ff88c0e3d64c71bbadb06
CNT-Mac-004 <cntmac004@CNT-Mac-004s-MacBook-Pro.local>
2026-02-07 18:45:31 +0530

    create blog added

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-// export const Base_Url="https://hamsfram.onrender.com"
-export const Base_Url="http://localhost:8000"
+export const Base_Url="https://hamsfram.onrender.com"
+// export const Base_Url="http://localhost:8000"
